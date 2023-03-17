019f709ecb13774e891b30746acb7dd39bced8b5
AnnaIemelianova <anna.170692@gmail.com>
2023-03-18 00:18:07 +0200

    add hw-10

--- src/index.js
+++ src/index.js
@@ -1,3 +1,48 @@
 import './css/styles.css';
+import { fetchCountries } from './js/fetchCountries';
+import debounce from 'lodash.debounce';
+import Notiflix from 'notiflix';
 
 const DEBOUNCE_DELAY = 300;
+const inputEl = document.querySelector('#search-box');
+const listEl = document.querySelector('.country-list');
+const countryData = document.querySelector('.country-info');
+
+inputEl.addEventListener('input', debounce(handleInputCountries, DEBOUNCE_DELAY));
+
+function handleInputCountries(event) {
+    const inputValue = inputEl.value.trim();
+    listEl.innerHTML = '';
+    countryData.innerHTML = '';
+
+    fetchCountries(inputValue)
+    .then(data => {
+        // console.log(data);
+        if(data.length > 10) {
+            Notiflix.Notify.warning('Too many matches found. Please enter a more specific name.');
+        }
+        else if(data.length > 2 && data.length < 10) {
+           const countiesListMarkup = data.reduce((acc, item) => acc + `<li>
+            <img src="${item.flags.svg}" alt="${item.name.official}" width="50" height="50">${item.name.official}
+            </li>`, '');
+            listEl.insertAdjacentHTML('beforeend', countiesListMarkup);
+        }
+        else if(data.length === 1) {
+            const dataCountryMarkup = data.reduce((acc, item) => acc + `
+            <img src="${item.flags.svg}" alt="${item.name.official}" width="50" height="50">
+            <h2>${item.name.official}</h2>
+            <p>Capital: ${item.capital}</p>
+            <p>Population: ${item.population}</p>
+            <p>Language: ${Object.values(item.languages)}</p>
+            `, '');
+            countryData.insertAdjacentHTML('beforeend', dataCountryMarkup);
+        }
+    })
+    .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name');
+    })
+};
+
+
+  
+
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,13 @@
+const BASE_URL = 'https://restcountries.com/v3.1/name';
+
+export const fetchCountries = (name) => {
+    return fetch(`${BASE_URL}/${name}?fields=name,capital,population,flags,languages`)
+    .then(response => {
+        if (response.status === 404) {
+            throw new Error(response.status);
+        };
+        // console.log(response);
+        return response.json();
+    })
+}
+
